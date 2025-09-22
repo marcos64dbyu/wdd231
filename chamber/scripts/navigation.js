612da1b695d7c05b3f8e3872d3186e3436c0baa6
marcos64dbyu <mer24031@bui.edu>
2025-09-22 12:45:14 -0500

    corrected css to chamber and principal page

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const nav = document.querySelector('nav');
+    const header = document.querySelector('header');
 
     hamburgerMenu.addEventListener('click', () => {
         nav.classList.toggle('open');
